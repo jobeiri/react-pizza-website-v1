c54a0e82b72ee67444441aee4a0dab38ab4434e4
Amir Hossein Jobeiri <jobeiri_a.h@yahoo.com>
2020-11-02 01:47:07 -0800

    Creating Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pizza</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
   );
 }
 
